d407e7417f5100c7210d746358aed4dd8aa1482c
RITESH ROUSHAN KUMAR <78962046+Ritesh1408@users.noreply.github.com>
2025-03-04 19:36:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
 //your JS code here. If required.
+let btns = document.querySelectorAll(".btn");
+let stop = document.querySelector(".stop");
+const audio = document.getElementById("audio");
+
+btns.forEach(btn => {
+    btn.addEventListener("click", () => {
+        audio.play();
+    });
+});
+
+stop.addEventListener("click", () => {
+   let audios = document.querySelectorAll("audio");
+    audios.forEach(audio => {
+        audio.pause();
+    });
+});
